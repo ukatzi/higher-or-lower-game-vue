7b06cb54a062f3650e8bd03a83b9004cebbcfd9f
ukatzi <ytalkov@gmail.com>
2026-03-04 19:45:48 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@ import {ref, watch} from 'vue';
 
 const nsfwContent = ref(false);
 const count = 100;
-const tag1 = ref(""), tag2 = ref(""), loading = ref("true"); 
+const anTime = 600, waitTime = 900;
+const anFrames = 50;
+const tag1 = ref(""), tag2 = ref(""), loading = ref("true"), firstLoad = ref("false"); 
 const href1 = ref(""), href2 = ref("");
 const count1 = ref(""), count2 = ref("");
+const countAn1 = ref("???"), countAn2 = ref("???");
+
 const er1 = ref("false"), er2 = ref("false");
 const score = ref("0"), maxScore = ref("0");
 
@@ -15,6 +19,15 @@ function rndGen(){
 async function updater(isCorrect){
   if(loading.value == false){ return;}
   loading.value = false;
+
+  if(firstLoad.value){
+    await Promise.all([
+      countAnimator1(),
+      countAnimator2()
+    ]);
+  }
+
+  console.log(123);
 
   if(isCorrect){
     score.value++;
@@ -26,15 +39,56 @@ async function updater(isCorrect){
   maxScore.value = Math.max(maxScore.value, score.value);
 
   var p1 = rndGen(), p2 = rndGen();
-  //alert(p1);
+  while(p2 == p1){
+    p2 = rndGen();
+  }
   await Promise.all([updater1(p1), updater2(p2)]);  
 
   await imageLoader();
 
+  firstLoad.value = true;
   loading.value = true;
   return;
 }
 
+async function countAnimator1(){
+  for(var i = 0; i < anFrames; i++){
+    await new Promise(function(resolve){
+      setTimeout(function(){
+        countAn1.value = Math.ceil((i + 1) * (count1.value / anFrames));
+        resolve(null);
+      }, anTime / anFrames)
+    });
+  }
+
+  await new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(null);
+    }, waitTime)
+  });
+
+  countAn1.value = "???";
+  return;
+}
+async function countAnimator2(){
+  for(var i = 0; i < anFrames; i++){
+    await new Promise(function(resolve){
+      setTimeout(function(){
+        countAn2.value = Math.ceil((i + 1) * (count2.value / anFrames));
+        resolve(null);
+      }, anTime / anFrames)
+    });
+  }
+
+  await new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(null);
+    }, waitTime)
+  });
+
+  countAn2.value = "???";
+  return;
+}
 
 async function imageLoader(){
   if(loading.value == false){ return;}
@@ -51,7 +105,7 @@ async function imageLoader1(){
   var post = await postF.json();
   
   if(post["posts"].length > 0){
-    href1.value = post["posts"][0]["main"]["file"];
+    href1.value = post["posts"][0]["preview"]["file"];
     er1.value = false;
   }
   else{
@@ -67,7 +121,7 @@ async function imageLoader2(){
   //console.log(post);
 
   if(post["posts"].length > 0){
-    href2.value = post["posts"][0]["main"]["file"];
+    href2.value = post["posts"][0]["preview"]["file"];
     er2.value = false;
   }
   else{
@@ -108,10 +162,11 @@ watch(nsfwContent, imageLoader);
         Max Score: {{ maxScore }}
       </div>
     </div>
-    <div v-if="loading" id="buttons"> 
+    <div v-if="firstLoad" id="buttons"> 
       <div id="firstChoose">
         <button @click="updater(count1 >= count2)" :disabled="!loading" id="firstButton">
           <h1> {{ tag1 }} </h1>
+          <h1> {{ countAn1 }}</h1>
           <img :src="href1" alt="Image couldn't load" v-if="!er1"/>
           <h1 v-else> Image couldn't load </h1>
         </button>
@@ -119,6 +174,7 @@ watch(nsfwContent, imageLoader);
       <div id="secondChoose">
         <button @click="updater(count2 >= count1)" :disabled="!loading" id="secondButton">
           <h1> {{ tag2 }}</h1>
+          <h1> {{ countAn2 }}</h1>
           <img :src="href2"  alt="Image couldn't load" v-if="!er2"/>
           <h1 v-else> Image couldn't load </h1>
         </button>
@@ -169,6 +225,7 @@ watch(nsfwContent, imageLoader);
   }
   #firstButton img{
     display: block;
+    min-width: 100%;
     max-width: 100%;
     height: auto;
   }
@@ -183,6 +240,7 @@ watch(nsfwContent, imageLoader);
   }
   #secondButton img{
     display: block;
+    min-width: 100%;
     max-width: 100%;
     height: auto;
   }
